0e7c21244b797d1eba28ca91d57f0f34969e367e
DouglasSenna <douglas.senna.cc@gmail.com>
2025-08-31 11:19:05 +0000

    primeiro commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const alimentacao = document.querySelector('#poke');
+const botao = document.querySelector('#power');
+const ledVermelho = document.querySelector("#ledVermelho");
+const ledAmarelo = document.querySelector("#ledAmarelo");
+const ledVerde = document.querySelector("#ledVerde");
+
+let ligado = false;
+
+function Ligar() {
+    ligado = !ligado;
+        botao.textContent = ligado ? "OFF" : "ON";
+            alimentacao.src = ligado ? "./img/pokeon.png" : "./img/pokeoff.png";
+
+                const leds = [
+                        {element: ledVermelho, classe: "piscando-red"},
+                                {element: ledAmarelo, classe: "piscando-yellow"},
+                                        {element: ledVerde, classe: "piscando-green"}
+                                            ];
+
+                                                leds.forEach(led => {
+                                                        if(ligado){
+                                                                    led.element.classList.add(led.classe);
+                                                                            } else {
+                                                                                        led.element.classList.remove(led.classe);
+                                                                                                    led.element.style.backgroundColor = "black";
+                                                                                                            }
+                                                                                                                });
+                                                                                                                }
+                                                                                                                
