efd668f9726218c21ce847b7429886d055d9ef8e
DouglasSenna <Douglas.senna.cc@gmail.com>
2025-08-31 08:59:32 -0300

    organizando identação

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const alimentacao = document.querySelector('#poke');
-const botao = document.querySelector('#power');
+const alimentacao = document.querySelector("#poke");
+const botao = document.querySelector("#power");
 const ledVermelho = document.querySelector("#ledVermelho");
 const ledAmarelo = document.querySelector("#ledAmarelo");
 const ledVerde = document.querySelector("#ledVerde");
@@ -7,23 +7,22 @@ const ledVerde = document.querySelector("#ledVerde");
 let ligado = false;
 
 function Ligar() {
-    ligado = !ligado;
-        botao.textContent = ligado ? "OFF" : "ON";
-            alimentacao.src = ligado ? "./img/pokeon.png" : "./img/pokeoff.png";
+  ligado = !ligado;
+  botao.textContent = ligado ? "OFF" : "ON";
+  alimentacao.src = ligado ? "./img/pokeon.png" : "./img/pokeoff.png";
 
-                const leds = [
-                        {element: ledVermelho, classe: "piscando-red"},
-                                {element: ledAmarelo, classe: "piscando-yellow"},
-                                        {element: ledVerde, classe: "piscando-green"}
-                                            ];
+  const leds = [
+    { element: ledVermelho, classe: "piscando-red" },
+    { element: ledAmarelo, classe: "piscando-yellow" },
+    { element: ledVerde, classe: "piscando-green" },
+  ];
 
-                                                leds.forEach(led => {
-                                                        if(ligado){
-                                                                    led.element.classList.add(led.classe);
-                                                                            } else {
-                                                                                        led.element.classList.remove(led.classe);
-                                                                                                    led.element.style.backgroundColor = "black";
-                                                                                                            }
-                                                                                                                });
-                                                                                                                }
-                                                                                                                
+  leds.forEach((led) => {
+    if (ligado) {
+      led.element.classList.add(led.classe);
+    } else {
+      led.element.classList.remove(led.classe);
+      led.element.style.backgroundColor = "black";
+    }
+  });
+}
